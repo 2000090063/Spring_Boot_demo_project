44435023c683a530cedbde0ac426a8fd62242981
veluru karthik_reddy <2000090063@kluniversity.in>
2022-04-02 20:41:00 +0530

    new spring-boot repo

---
+++ WebApp/src/main/java/com/edureka/hello_world/CoursesController.java
@@ -0,0 +1,16 @@
+package com.edureka.hello_world;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class CoursesController {
+
+@RequestMapping("/tryhard")
+public String courses ()
+{
+		
+		return "course.jsp";
+}
+
+}
---
+++ WebApp/src/test/java/com/edureka/hello_world/WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edureka.hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
